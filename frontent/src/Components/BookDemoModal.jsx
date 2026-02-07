1b54317837946d93bf360a7885fa3fe8e1c534fc
dinesh <dineshkumarp1206@gmail.com>
2026-02-07 20:46:50 +0530

    update demopage

--- frontent/src/Components/BookDemoModal.jsx
+++ frontent/src/Components/BookDemoModal.jsx
@@ -192,7 +192,7 @@ export default function BookDemoModal({ open, onClose }) {
           >
             {loading ? (
               <>
-                <Loader2 className="h-4 w-4 animate-spin" />
+                {/* <Loader2 className="h-4  w-4 animate-spin" /> */}
                 Processing...
               </>
             ) : (
