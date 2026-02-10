cc2516a2296fd865d699f593669312be56ed9c59
dinesh <dineshkumarp1206@gmail.com>
2026-02-10 14:07:43 +0530

    update

--- frontent/src/Home/BannerSection.jsx
+++ frontent/src/Home/BannerSection.jsx
@@ -179,7 +179,7 @@ const handleMouseLeave = () => {
 
       {/* RIGHT SIDE BUTTON */}
       <div className="flex justify-center lg:justify-end items-center lg:pr-38">
-        <a  target="_blank" rel="noopener noreferrer" href="https://hello-aumne-production.up.railway.app/">
+        <a  target="_blank" rel="noopener noreferrer" href="https://hello.aumne.ai/">
           <button
             className="
               px-14 sm:px-20 py-6 sm:py-9
